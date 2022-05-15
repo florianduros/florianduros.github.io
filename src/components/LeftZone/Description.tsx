53af93b025ee4a152cd29eba557a36f82c7c7c9a
Florian Duros <florian.duros@gmail.com>
2022-05-15 16:03:53 +0200

    add profile

--- src/components/LeftZone/Description.tsx
+++ src/components/LeftZone/Description.tsx
@@ -4,11 +4,9 @@ export function Description() {
   return (
     <Container text="Profile">
       <p className="text-justify">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries,
+        I'm a software engineer with frontend and backend experiences built over
+        the last years. Currently, I'm mainly working on the frontend area with
+        a strong focus on the accessibility and the user experience.
       </p>
     </Container>
   );
