b0ae6c0115e9e7c4c584dc7c969cd19d98247389
Florian Duros <florian.duros@gmail.com>
2022-05-15 20:39:38 +0200

    improve design

--- src/components/Header/Photo.tsx
+++ src/components/Header/Photo.tsx
@@ -1,7 +1,7 @@
 export function Photo() {
   return (
     <img
-      className="rounded-lg border-2 border-sky-100 px-3"
+      className="bg-zinc-600 rounded-lg border-2 border-sky-100 px-3"
       src="./me.png"
       alt="Florian Duros"
       style={{
