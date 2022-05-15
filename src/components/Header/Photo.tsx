db669fe8cc1f8ac3d51e9ec9700c23aeb508dd1e
Florian Duros <florian.duros@gmail.com>
2022-05-15 16:26:04 +0200

    new color

--- src/components/Header/Photo.tsx
+++ src/components/Header/Photo.tsx
@@ -1,12 +1,11 @@
 export function Photo() {
   return (
     <img
-      className="bg-orange-200 rounded-lg border-2 border-orange-100 px-3"
+      className="rounded-lg border-2 border-sky-100 px-3"
       src="./me.png"
       alt="Florian Duros"
       style={{
         height: 120,
-        backgroundColor: "#ffb15b",
       }}
     />
   );
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -4,7 +4,7 @@ type TitleProps = {
 
 export function Title({ title }: TitleProps) {
   return (
-    <h3 className="border-b-2 border-amber-600 desktop:text-3xl mobile:text-xl desktop:pb-2 desktop:mb-4 mobile:mb-2">
+    <h3 className="border-b-2 border-sky-500 desktop:text-3xl mobile:text-xl desktop:pb-2 desktop:mb-4 mobile:mb-2">
       {title}
     </h3>
   );
